9a2013b22bd7947aa1c2b4c05ab6e02518347f11
Hannah Howard <hannah@hannahhoward.net>
2016-04-07 15:23:10 -0700

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'tweets#index'
   resources :users, only:[:new, :create]
   resources :tweets
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login', to: 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
