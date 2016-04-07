d7c6aa3804f8b817fdfa48d16a3d41a1bfa5bfbf
Hannah Howard <hannah@hannahhoward.net>
2016-04-07 15:58:32 -0700

    authorize"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authorize
+    if current_user.nil?
+      redirect_to login_path, alert: 'Not authorized - you must be logged in!'
+    end
+  end
+
 end
